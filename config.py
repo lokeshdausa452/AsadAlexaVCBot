656ed1313650f70ca720ee181b2bc127eae2ed36
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-12-28 10:38:42 +0500

    Alexa Vc Userbot

--- config.py
+++ config.py
@@ -14,8 +14,6 @@ SESSION = os.getenv("SESSION")
 HNDLR = os.getenv("HNDLR", "/")
 GROUP_MODE = os.getenv("GROUP_MODE", "True")
 
-IMG_1 = os.getenv("IMG_1", "https://telegra.ph/file/e877179fe18a04192a00a.jpg")
-IMG_2 = os.getenv("IMG_2", "https://telegra.ph/file/74d45f53fc287e9c316d1.jpg")
 
 contact_filter = filters.create(
     lambda _, __, message:
